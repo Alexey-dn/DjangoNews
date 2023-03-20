a4125edf3b8fd93cd76f0f2565dba12efe6b7849
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-03-20 19:11:13 +0300

    Registered model

--- News/chitchat/admin.py
+++ News/chitchat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author, Post, Category, Comment
 
-# Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Post)  # Не забываем зарегистрировать модели, иначе мы не увидим их в админке.
+admin.site.register(Category)
+admin.site.register(Comment)
+
