21f3ba3ac9799f915079793779919b81bc2abc07
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-03-23 08:10:01 +0300

    add classes to display singular & plural models name in admin panel

--- News/chitchat/models.py
+++ News/chitchat/models.py
@@ -26,14 +26,22 @@ class Author(models.Model):
         self.save()
 
     def __str__(self):
-        return f'{self.authorUser}'
+        return self.authorUser
+
+    class Meta:
+        verbose_name = 'автор'
+        verbose_name_plural = 'авторы'
 
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
 
 
 class Post(models.Model):
@@ -67,6 +75,10 @@ class Post(models.Model):
     def __str__(self):
         return f'{self.text}'
 
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -90,3 +102,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'{self.comment[0:40]}'
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
