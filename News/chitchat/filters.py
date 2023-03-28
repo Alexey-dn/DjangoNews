1312ecb390606acab9fdcc0115299f9b3e8222cd
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-03-28 22:04:13 +0300

    made search filter

--- News/chitchat/filters.py
+++ News/chitchat/filters.py
@@ -1,4 +1,5 @@
-from django_filters import FilterSet, ModelChoiceFilter
+from django_filters import FilterSet, ModelChoiceFilter, DateTimeFilter, CharFilter
+from django.forms import DateTimeInput
 from .models import Post, Category
 
 
@@ -7,6 +8,11 @@ from .models import Post, Category
 # должен чем-то напомнить знакомые вам Django дженерики.
 
 class PostFilter(FilterSet):
+    title = CharFilter(
+        lookup_expr='icontains',
+        field_name='title',
+        label='Заголовок',
+    )
 
     category = ModelChoiceFilter(
         field_name='postcategory__category',
@@ -14,21 +20,15 @@ class PostFilter(FilterSet):
         label='Категория',
         empty_label='Любая',
     )
+    date = DateTimeFilter(
+        field_name='time_creation',
+        lookup_expr='gt',
+        label='Дата',
+        widget=DateTimeInput(
+            attrs={'type': 'date'},
+        ),
+    )
 
     class Meta:
-        # В Meta классе мы должны указать Django модель,
-        # в которой будем фильтровать записи.
         model = Post
-        # В fields мы описываем по каким полям модели
-        # будет производиться фильтрация.
-        fields = {
-            # поиск по названию
-            'title': ['icontains'],
-            # 'postcategory__category': ['exact'],
-            # количество товаров должно быть больше или равно
-            # 'quantity': ['gt'],
-            # 'price': [
-            #     'lt',  # цена должна быть меньше или равна указанной
-            #     'gt',  # цена должна быть больше или равна указанной
-            # ],
-        }
+        fields = ['title', 'date', 'category']
