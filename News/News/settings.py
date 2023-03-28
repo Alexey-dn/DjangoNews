381e0e237da731e9b52f3247c511ccb2273f2b66
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-03-28 22:08:34 +0300

    add warnings (don't sure about purpose)

--- News/News/settings.py
+++ News/News/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import warnings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+warnings.filterwarnings(
+    'error', r"DateTimeField .* received a naive datetime",
+    RuntimeWarning, r'django.db.models.fields',
+)
