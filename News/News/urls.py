97acfd1ee51f90d9e232e1bc3b9652782d80b579
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-02 22:09:24 +0300

    add path for email login and commented path for username login

--- News/News/urls.py
+++ News/News/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path("accounts/", include("accounts.urls")),  # Важен порядок с предыдущим путем
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("allauth.urls")),
     path('pages/', include('django.contrib.flatpages.urls')),
     path('news/', include('chitchat.urls')),
 ]
