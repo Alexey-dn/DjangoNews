965cef588ea16b0cc9a533407bb8e2dc53ee52a8
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-10 14:38:59 +0300

    add mailsend after user registration and admin notification of user registration mailing

--- News/accounts/forms.py
+++ News/accounts/forms.py
@@ -1,20 +1,30 @@
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+from django.core.mail import mail_admins
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="Email")
-    first_name = forms.CharField(label="Имя")
-    last_name = forms.CharField(label="Фамилия")
+class CustomSignupForm(SignupForm):
+    first_name = forms.CharField(label="Имя", required=False)
 
     class Meta:
         model = User
         fields = (
-            "username",
             "first_name",
-            "last_name",
             "email",
             "password1",
             "password2",
         )
+
+    def save(self, request):
+        user = super().save(request)
+        common_users = Group.objects.get(name="common users")
+        user.groups.add(common_users)
+
+        mail_admins(
+            subject='Новый пользователь!',
+            message=f'Пользователь {user.first_name} зарегистрировался на сайте.'
+        )
+
+        return user
