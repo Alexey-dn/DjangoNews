0fa9a7472053bc7304d2aa305f5ee9bd63e4ad9f
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-20 19:03:33 +0300

    Revert "add celery timezone"
    This reverts commit 0414e654413f3d15b2ad0778bb032840774903d0.

--- News/News/celery.py
+++ News/News/celery.py
@@ -1,7 +1,5 @@
 import os
 from celery import Celery
-from celery.schedules import crontab
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'News.settings')
 
@@ -9,11 +7,3 @@ app = Celery('News')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    'action_every_monday_morning': {
-        'task': 'chitchat.tasks.new_post_notification',
-        'schedule': crontab(minute="0",  hour="18", day_of_week="fri")
-        # 'schedule': 30,
-    },
-}
