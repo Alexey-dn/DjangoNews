a836406f836c78e310bf722a5e97235d938fb8d4
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-23 15:38:54 +0300

    del useless cache method

--- News/chitchat/urls.py
+++ News/chitchat/urls.py
@@ -6,8 +6,6 @@ from .views import (
 )
 from .views import IndexView
 
-# кэширование
-from django.views.decorators.cache import cache_page
 
 
 urlpatterns = [
@@ -17,8 +15,8 @@ urlpatterns = [
     # Т.к. наше объявленное представление является классом,
     # а Django ожидает функцию, нам надо представить этот класс в виде view.
     # Для этого вызываем метод as_view.
-    path('', cache_page(60)(PostList.as_view()), name='post_list'),
-    path('<int:pk>', cache_page(60*5)(PostDetail.as_view()), name='post_detail'),
+    path('', PostList.as_view(), name='post_list'),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     # Если дженерик в файле views.py представлен в виде класса то в path
     # обработчик записывается в виде as_view(), а если представлен функцией,
     # то PostDetail.название функции, которую мы предварительно импортируем
