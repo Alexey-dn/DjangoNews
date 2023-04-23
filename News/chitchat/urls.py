1c387692af4b8bb0eee8ed1de24c0e2f29d31daa
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-04-23 14:35:29 +0300

    add caching for several pages

--- News/chitchat/urls.py
+++ News/chitchat/urls.py
@@ -6,6 +6,9 @@ from .views import (
 )
 from .views import IndexView
 
+# кэширование
+from django.views.decorators.cache import cache_page
+
 
 urlpatterns = [
     # path — означает путь.
@@ -14,8 +17,8 @@ urlpatterns = [
     # Т.к. наше объявленное представление является классом,
     # а Django ожидает функцию, нам надо представить этот класс в виде view.
     # Для этого вызываем метод as_view.
-    path('', PostList.as_view(), name='post_list'),
-    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('', cache_page(60)(PostList.as_view()), name='post_list'),
+    path('<int:pk>', cache_page(60*5)(PostDetail.as_view()), name='post_detail'),
     # Если дженерик в файле views.py представлен в виде класса то в path
     # обработчик записывается в виде as_view(), а если представлен функцией,
     # то PostDetail.название функции, которую мы предварительно импортируем
