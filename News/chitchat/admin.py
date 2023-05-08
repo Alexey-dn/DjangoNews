9587f90653122933ffebd775e671557998920efd
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-05-08 12:46:31 +0300

    add custom fields view in admin panel

--- News/chitchat/admin.py
+++ News/chitchat/admin.py
@@ -2,8 +2,27 @@ from django.contrib import admin
 from .models import Author, Post, Category, Comment
 
 
-admin.site.register(Author)
-admin.site.register(Post)  # Не забываем зарегистрировать модели, иначе мы не увидим их в админке.
+class PostAdmin(admin.ModelAdmin):
+    # list_display = [field.name for field in Post._meta.get_fields()]
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    # генерируем список имён всех полей для более красивого отображения
+    list_display = ('title', 'rating')  # если нужны не все поля, оставляем только необходимые
+    # list_filter = ('price', 'quantity', 'name')  # добавляем примитивные фильтры в нашу админку
+    search_fields = ('title', 'text')  # тут всё очень похоже на фильтры из запросов в базу
+
+
+class PostAuthor(admin.ModelAdmin):
+    list_display = ('authorUser', 'ratingAuthor')
+    search_fields = ('authorUser', 'ratingAuthor')
+
+
+class PostComment(admin.ModelAdmin):
+    list_display = ('comment', 'user_comment', 'post_comment')
+    search_fields = ('post_comment', 'user_comment')
+
+
+admin.site.register(Author, PostAuthor)
+admin.site.register(Post, PostAdmin)  # Не забываем зарегистрировать модели, иначе мы не увидим их в админке.
 admin.site.register(Category)
-admin.site.register(Comment)
+admin.site.register(Comment, PostComment)
 
