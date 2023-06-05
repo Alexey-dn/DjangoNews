76fdccacfaac3a8fccace156807f3a2603277eb3
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-06-05 11:51:13 +0300

    add translation method

--- News/chitchat/models.py
+++ News/chitchat/models.py
@@ -2,13 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Sum
 from django.urls import reverse
+from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy
 
 from django.core.cache import cache  # кеширование
 
 
 class Author(models.Model):
-    authorUser = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='автор')
-    ratingAuthor = models.IntegerField(default=0, verbose_name='рейтинг')
+    authorUser = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=gettext_lazy('Author'),
+    )
+    ratingAuthor = models.IntegerField(
+        default=0,
+        verbose_name=gettext_lazy('Rating'),
+    )
 
     def update_rating(self):
         postRate = self.post_set.aggregate(postRating=Sum('rating')) #  метод _set позволяет реверсивно обратиться
@@ -20,11 +29,6 @@ class Author(models.Model):
         c_r = 0
         c_r += commentRate.get("commentRating")
 
-       # c_p = 0
-       #  for post in self.post_set.all():
-       #      commentPostRate = post.comment_set.aggregate(commentPostRating=Sum('rating'))
-       #      c_p += commentPostRate.get('commentPostRating')
-
         self.ratingAuthor = p_r * 3 + c_r
         self.save()
 
@@ -32,20 +36,25 @@ class Author(models.Model):
         return f'{self.authorUser}'
 
     class Meta:  #  Возвращает название категории во множественном или единственном числе в админпанеле
-        verbose_name = 'автор'
-        verbose_name_plural = 'авторы'
+        verbose_name = _('Author')
+        verbose_name_plural = _('Authors')
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True, verbose_name='название')
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name=gettext_lazy('Category'),
+        help_text=gettext_lazy('Enter a category name'),
+    )
     users = models.ManyToManyField(User, through='Subscription')
 
     def __str__(self):
         return f'{self.name.title()}'
 
     class Meta:
-        verbose_name = 'категория'
-        verbose_name_plural = 'категории'
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
 
 
 class Post(models.Model):
@@ -53,21 +62,32 @@ class Post(models.Model):
     article = 'A'
 
     POST_TYPES = [
-        (news, 'Новость'),
-        (article, 'Статья')
+        (news, gettext_lazy('News')),
+        (article, gettext_lazy('Article'))
     ]
 
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name='автор')
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        verbose_name=gettext_lazy('Author'),
+    )
     #  verbose_name определяет имя поля, которое будет отображаться в админ панели внутри каждой модели
     post_type = models.CharField(
-        max_length=1, choices=POST_TYPES,
-        default=article, verbose_name='тип новости'
+        max_length=1,
+        choices=POST_TYPES,
+        default=article,
+        verbose_name=gettext_lazy('Post type'),
+        help_text=gettext_lazy('Choose a post type'),
     )
     time_creation = models.DateTimeField(auto_now_add=True)
-    post_category = models.ManyToManyField(Category, through='PostCategory')
-    title = models.CharField(max_length=100, unique=True, verbose_name='заголовок')
-    text = models.TextField(verbose_name='текст')
-    rating = models.IntegerField(default=0, verbose_name='рейтинг')
+    post_category = models.ManyToManyField(Category, through='PostCategory', verbose_name=gettext_lazy('Category'))
+    title = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name=gettext_lazy('Heading'),
+    )
+    text = models.TextField(verbose_name=gettext_lazy('Text'))
+    rating = models.IntegerField(default=0, verbose_name=gettext_lazy('Rating'))
 
     def like(self):
         self.rating += 1
@@ -91,8 +111,8 @@ class Post(models.Model):
         cache.delete(f'post-{self.pk}')  # затем удаляем его из кэша, чтобы сбросить его
 
     class Meta:
-        verbose_name = 'новость'
-        verbose_name_plural = 'новости'
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
 
 
 class PostCategory(models.Model):
@@ -101,11 +121,22 @@ class PostCategory(models.Model):
 
 
 class Comment(models.Model):
-    post_comment = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='статья')
-    user_comment = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='автор')
-    comment = models.TextField(verbose_name='комментарий')
+    post_comment = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        verbose_name=gettext_lazy('Post'),
+    )
+    user_comment = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name=gettext_lazy('Author'),
+    )
+    comment = models.TextField(verbose_name=gettext_lazy('Comment'))
     time_creation = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField(default=0, verbose_name='рейтинг')
+    rating = models.IntegerField(
+        default=0,
+        verbose_name=gettext_lazy('Rating'),
+    )
 
     def like(self):
         self.rating += 1
@@ -119,8 +150,8 @@ class Comment(models.Model):
         return f'{self.comment[0:40]}'
 
     class Meta:
-        verbose_name = 'комментарий'
-        verbose_name_plural = 'комментарии'
+        verbose_name = _('Comment')
+        verbose_name_plural = _('Comments')
 
 
 class Subscription(models.Model):
@@ -136,5 +167,5 @@ class Subscription(models.Model):
     )
 
     class Meta:
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
+        verbose_name = _('Subscription')
+        verbose_name_plural = _('Subscriptions')
