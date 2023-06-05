d1e8254bc87f92a9814d6391eb02c36808160ed6
Alexey-dn <121422412+Alexey-dn@users.noreply.github.com>
2023-06-05 11:54:22 +0300

    added translation

--- News/chitchat/management/commands/postsdel.py
+++ News/chitchat/management/commands/postsdel.py
@@ -1,18 +1,19 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import gettext_lazy as _
 from chitchat.models import Post, Category
 
 
 class Command(BaseCommand):
-    help = 'Подсказка вашей команды'
+    help = _('Your command hint')
 
     def add_arguments(self, parser):
         parser.add_argument('category', type=str)
 
     def handle(self, *args, **options):
-        answer = input(f'Вы правда хотите удалить все статьи в категории {options["category"]}? yes/no \n')
+        answer = input(_('Do you really want to delete all articles in a category') + f'{options["category"]}' + _('? yes/no \n'))
 
-        if answer != 'yes':
-            self.stdout.write(self.style.ERROR('Отменено'))
+        if answer != 'yes' or answer != 'да':
+            self.stdout.write(self.style.ERROR(_('Canceled')))
 
         try:
             category = Category.objects.get(name=options['category'])
