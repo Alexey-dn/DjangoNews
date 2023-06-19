413c30c5665d5a47ab5408385f45107f41a5b71a
Alexey-dn <ilyukhin1981@internet.ru>
2023-06-19 15:34:34 +0300

    some correction

--- News/chitchat/urls.py
+++ News/chitchat/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # Для этого вызываем метод as_view.
     path('', PostList.as_view(), name='post_list'),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
-    # Если дженерик в файле views.py представлен в виде класса то в path
+    # Если дженерик в файле views.py представлен в виде класса, то в path
     # обработчик записывается в виде as_view(), а если представлен функцией,
     # то PostDetail.название функции, которую мы предварительно импортируем
     path('create/', PostCreate.as_view(), name='post_create'),
